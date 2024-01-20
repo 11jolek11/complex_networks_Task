b45042813cc4558d8d7b435c04f626af9b57ba22
11jolek11 <dabrowskiandrzej094@gmail.com>
2024-01-20 21:15:16 +0100

    import

--- project/recv.py
+++ project/recv.py
@@ -1,213 +1,192 @@
-import random
-from copy import copy
-
-import requests
 import networkx as nx
-import pandas as pd
+import requests
 import matplotlib.pyplot as plt
-import numpy as np
-import ndlib.models.ModelConfig as mc
-import ndlib.models.epidemics as ep
-from ndlib.viz.mpl.DiffusionTrend import DiffusionTrend
-from statistics import mean
-import seaborn as sns
-from networkx.algorithms.community import modularity
-from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
-from matplotlib import cm
+import matplotlib.animation as animation
+import matplotlib.widgets as widgets
+import matplotlib
+matplotlib.use("TkAgg")
+
+class ClientApp:
+    def __init__(self):
+        # Inicjalizacja pustego grafu
+        self.graph = nx.Graph()
+        # Pamięć dla nodów
+        self.node_mem = []
+
+        # Utworzenie obiektu figure i dwóch osi (ax1 i ax2)
+        self.fig, (self.ax1, self.ax2) = plt.subplots(1, 2, figsize=(10, 5))
+
+        # Ustawienia dla ax1 (graf)
+        self.ax1.set_title('Graph Animation')
+
+        # Ustawienia dla ax2 (tekst)
+        self.ax2.set_title('Graph Metrics')
+        self.ax2.axis('off')  # Wyłączenie wyświetlania osi
+
+        # Dodatkowy tekst na wykresie
+        self.texts = {
+            'order': self.ax2.text(0.1, 0.9, 'Order: ', fontsize=10),
+            'size': self.ax2.text(0.1, 0.85, 'Size: ', fontsize=10),
+            'density': self.ax2.text(0.1, 0.8, 'Density: ', fontsize=10),
+            'diameter': self.ax2.text(0.1, 0.75, 'Diameter: ', fontsize=10),
+            'radius': self.ax2.text(0.1, 0.7, 'Radius: ', fontsize=10),
+            'avg_shortest_path': self.ax2.text(0.1, 0.65, 'Avg Shortest Path: ', fontsize=10),
+            'avg_degree': self.ax2.text(0.1, 0.6, 'Avg Degree: ', fontsize=10),
+            'clustering_coefficient': self.ax2.text(0.1, 0.55, 'Clustering Coefficient: ', fontsize=10),
+            'connectivity_coefficient': self.ax2.text(0.1, 0.5, 'Connectivity Coefficient: ', fontsize=10),
+            'avg_centrality': self.ax2.text(0.1, 0.45, 'Avg Centrality: ', fontsize=10),
+            'degree_correlation_coefficient': self.ax2.text(0.1, 0.4, 'Degree Correlation Coefficient: ', fontsize=10),
+            'avg_closeness': self.ax2.text(0.1, 0.35, 'Avg Closeness: ', fontsize=10),
+        }
+
+        self.weight = 0
+
+    def fetch_graph_from_server(self):
+        # Zapytanie do serwera
+        server_url = "http://127.0.0.1:5000/get"  # Aktualizuj, jeśli serwer działa na innym hoście lub porcie
+        response = requests.get("http://127.0.0.1:5000/get")
+
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            print("Failed to fetch graph from server.")
+            return {'nodes': [], 'edges': []}
+
+    def update_graph(self, graph_data):
+        # Wyczyszczenie istniejącego grafu
+        self.graph.clear()
+
+        # Dodanie wierzchołków i krawędzi do grafu
+        for node_data in graph_data['nodes']:
+            node_id = node_data['id']
+            node_weight = node_data.get('weight', 0)  # Pobierz wagę, jeśli istnieje, w przeciwnym razie ustaw na 0
+            self.graph.add_node(node_id, weight=node_weight)
+
+        self.graph.add_edges_from(graph_data['edges'])
+
+        # Usunięcie wierzchołków o wadze mniejszej niż self.weight
+        nodes_to_remove = [node for node in self.graph.nodes if self.graph.nodes[node].get('weight', 0) < self.weight]
+        self.graph.remove_nodes_from(nodes_to_remove)
+
+    def animate(self, frame):
+        # Pobranie grafu z serwera
+        graph_data_from_server = self.fetch_graph_from_server()
+
+        # Sprawdzenie, czy struktura grafu się zmieniła
+        if graph_data_from_server['nodes'] != self.node_mem:
+
+            # Zapisanie nowej struktury do pamięci
+            self.node_mem = graph_data_from_server['nodes']
+
+            # Zaktualizuj graf
+            self.update_graph(graph_data_from_server)
+
+            # Wyczyść ax1 i narysuj nowy graf
+            self.ax1.clear()
+            nx.draw(self.graph, with_labels=True, font_size=8, node_size=50, ax=self.ax1)
+
+            # Aktualizacja tekstu w ax2
+            graph_metrics = self.calculate_graph_metrics()
+            for key, text_object in self.texts.items():
+                text_object.set_text(f'{key.capitalize()}: {graph_metrics[key]}')
 
 
+    def calculate_graph_metrics(self):
+        # Rząd grafu (liczba wierzchołków)
+        order = len(self.graph.nodes())
 
-class GraphEngine():
-    def __init__(self) -> None:
-        self._graph_storage = nx.Graph()
+        # Rozmiar grafu (liczba krawędzi)
+        size = len(self.graph.edges())
 
-    def add_data(self, new_data):
-        self._graph_storage.add_edges_from(new_data)
+        # Gęstość grafu
+        density = nx.density(self.graph)
 
-    def all_nodes_pagerank(self):
-        my_dict = nx.pagerank(self)
-        my_frame = pd.DataFrame({"Node": list(my_dict.keys()), "Rank": list(my_dict.values())})
-        my_frame.index.name = "Index"
-        sns.displot(my_frame, x="Rank", kde=True)
+        # Średnica grafu
+        try:
+            diameter = nx.diameter(self.graph)
+        except nx.NetworkXError:
+            diameter = float("inf")
 
-    def attack(self, max_iters=20):
-        G_copy = copy(self._graph_storage)
-        G_size = self._graph_storage.number_of_nodes()
+        # Promień grafu
+        try:
+            radius = nx.radius(self.graph)
+        except nx.NetworkXError:
+            radius = float("inf")
 
-        centr = nx.degree_centrality(self._graph_storage)
-        dcs = pd.Series(centr)
-        dcs.sort_values(ascending=False, inplace=True)
+        # Średnia najkrótsza ścieżka
+        try:
+            avg_shortest_path = nx.average_shortest_path_length(self.graph)
+        except nx.NetworkXError:
+            avg_shortest_path = float("inf")
 
-        removal_propor = []
-        diameter_hist = []
+        # Stopień grafu
+        degree_values = list(dict(self.graph.degree()).values())
+        avg_degree = sum(degree_values) / order
 
-        current_removal_propor = 0.0
+        # Współczynnik skupienia grafu
+        clustering_coefficient = nx.average_clustering(self.graph)
 
-        dcs = dcs.index.values.tolist()
+        # Współczynnik spójności grafu
+        connected_components = list(nx.connected_components(self.graph))
+        connectivity_coefficient = len(connected_components)
 
-        for i in range(len(dcs[:max_iters])):
-            G_copy.remove_node(dcs[i])
+        # Współczynnik centralizacji grafu
+        centrality_coefficients = nx.degree_centrality(self.graph).values()
+        avg_centrality = sum(centrality_coefficients) / order
 
-            comps = list((G_copy.subgraph(c) for c in nx.connected_components(self._graph_storage)))
-            diameter_hist.append(mean([nx.diameter(comp.to_undirected()) for comp in comps]))
+        # Współczynnik korelacji stopniowej grafu
+        try:
+            degree_correlation_coefficient = nx.degree_pearson_correlation_coefficient(self.graph)
+        except Exception:
+            degree_correlation_coefficient = None
 
-            current_removal_propor = 1 - (G_copy.number_of_nodes()/G_size)
-            removal_propor.append(current_removal_propor)
-        print(f"Diameter len {len(diameter_hist)}")
-        print(f"Diameter len {len(removal_propor)}")
+        # Współczynnik bliskości grafu
+        closeness_coefficients = nx.closeness_centrality(self.graph).values()
+        avg_closeness = sum(closeness_coefficients) / order
 
-        plt.plot(removal_propor, diameter_hist)
-        plt.plot()
+        # Zwróć obliczone parametry
+        return {
+            'order': order,
+            'size': size,
+            'density': density,
+            'diameter': diameter,
+            'radius': radius,
+            'avg_shortest_path': avg_shortest_path,
+            'avg_degree': avg_degree,
+            'clustering_coefficient': clustering_coefficient,
+            'connectivity_coefficient': connectivity_coefficient,
+            'avg_centrality': avg_centrality,
+            'degree_correlation_coefficient': degree_correlation_coefficient,
+            'avg_closeness': avg_closeness
+        }
 
-        plt.clf()
-
-    def fail(self, max_iters=20):
-        G_copy = copy(self._graph_storage)
-        G_size = self._graph_storage.number_of_nodes()
-
-        centr = nx.degree_centrality(self._graph_storage)
-        dcs = pd.Series(centr)
-        dcs.sort_values(ascending=False, inplace=True)
-
-        removal_propor = []
-        diameter_hist = []
-
-        current_removal_propor = 0.0
-
-        dcs = dcs.index.values.tolist()
-
-        current_iter = 0
-
-        while max_iters >= current_iter:
-            print(f"Fail iter: {current_iter}")
-            current_iter += 1
-
-            node_for_removal = dcs.pop(random.randrange(len(dcs)))
-            print(f"node_for_removal {node_for_removal}")
-
-            G_copy.remove_node(node_for_removal)
-
-            comps = list((G_copy.subgraph(c) for c in nx.connected_components(self._graph_storage)))
-
-            te = "Size of cc: "
-            for comp in comps:
-                te += str(comp.number_of_nodes()) + " "
-            print(te)
-
-            diameter_hist.append(mean([nx.diameter(comp.to_undirected()) for comp in comps]))
-
-            current_removal_propor = 1 - (G_copy.number_of_nodes()/G_size)
-            removal_propor.append(current_removal_propor)
-
-        print(f"Diameter len {len(diameter_hist)}")
-        print(f"Diameter len {len(removal_propor)}")
-
-        plt.plot(removal_propor, diameter_hist)
-        plt.show()
-        plt.clf()
-
-    def epidemy(self, model_params: dict, n: int):
-        model = ep.SEIRModel(self._graph_storage)
-
-        cfg = mc.Configuration()
-
-        for item in model_params.items():
-            cfg.add_model_parameter(item[0], item[1])
-
-        model.set_initial_status(cfg)
-        iterations = model.iteration_bunch(n)
-
-        trends = model.build_trends(iterations)
-        viz = DiffusionTrend(model, trends)
-        viz.plot("diffusion")
-        plt.show()
-
-    def degree_distribution(self):
-        degree_sequence = sorted((d for _, d in self._graph_storage.degree()), reverse=True)
-
-        fig = plt.figure("Degree of a random graph", figsize=(8, 8))
-        # Create a gridspec for adding subplots of different sizes
-        axgrid = fig.add_gridspec(5, 4)
-
-        ax0 = fig.add_subplot(axgrid[0:3, :])
-        Gcc = self._graph_storage.subgraph(sorted(nx.connected_components(self._graph_storage), key=len, reverse=True)[0])
-        pos = nx.spring_layout(Gcc, seed=10396953)
-        nx.draw_networkx_nodes(Gcc, pos, ax=ax0, node_size=20)
-        nx.draw_networkx_edges(Gcc, pos, ax=ax0, alpha=0.4)
-        ax0.set_title("Connected components of self._graph_storage")
-        ax0.set_axis_off()
-
-        ax1 = fig.add_subplot(axgrid[3:, :2])
-        ax1.plot(degree_sequence, "b-", marker="o")
-        ax1.set_title("Degree Rank Plot")
-        ax1.set_ylabel("Degree")
-        ax1.set_xlabel("Rank")
-
-        ax2 = fig.add_subplot(axgrid[3:, 2:])
-        ax2.bar(*np.unique(degree_sequence, return_counts=True))
-        ax2.set_title("Degree histogram")
-        ax2.set_xlabel("Degree")
-        ax2.set_ylabel("# of Nodes")
-
-        fig.tight_layout()
-        print("plot")
-        plt.plot()
-        plt.show()
-
-    def agl_methods(self, method: str):
-        floyd = nx.floyd_warshall_numpy(self._graph_storage)
-        linked = linkage(floyd, method=method)
-        print(floyd)
-        # Dendrogram
-        plt.figure(figsize=(10, 7))
-        dendrogram(linked, orientation='top')
-        plt.show()
-
-    def plot_graph(self):
-        plt.clf()
-        self._graph_storage
-        nx.draw(self._graph_storage)
-        plt.plot()
-        plt.show()
-
-    def divisive(self, method: str, threshold=3, criterion: str = "distance"):
-        adj_matrix = nx.to_numpy_array(self._graph_storage)
-        modularity_div = {}
-
-        meth = "complete"
-        divisive_clusters = linkage(adj_matrix.T, method=meth)
-        divisive_labels = fcluster(divisive_clusters, t=threshold, criterion=criterion)
-
-        for node, cluster in zip(self._graph_storage.nodes, divisive_labels):
-            self._graph_storage.nodes[node]['div'] = cluster
-
-        _, (ax1, ax2) = plt.subplots(1, 2, figsize=(18, 12))
-        pos = nx.kamada_kawai_layout(self._graph_storage)
-        cmap = cm.get_cmap('hsv', max(divisive_labels))
-
-        nx.draw(self._graph_storage, pos, node_color=[cmap(i) for i in divisive_labels], with_labels=False, ax=ax1)
-        ax1.set_title(f"Metoda podziałowa {meth}")
-
-        dendrogram(divisive_clusters, ax=ax2, no_labels=True)
-        ax2.set_title(f"Dendrogram {meth}")
+    def run(self, interval=10000):
+        nx.draw(self.graph, with_labels=True, font_size=8, node_size=50, ax=self.ax1)
+        self.ani = animation.FuncAnimation(self.fig, self.animate, interval=interval)
         plt.show()
 
-        modularity_div[meth] = modularity(self._graph_storage, [{node for node, data in self._graph_storage.nodes(data=True) if data['div'] == cluster} for cluster in set(divisive_labels)])
-        print(f"Divisive: {modularity_div}")
+def define_text_boxes(app):
+    interval_box_ax = plt.axes([0.62, 0.30, 0.2, 0.05])
+    interval_box = widgets.TextBox(interval_box_ax, 'Interval:')
+    interval_box.on_submit(lambda text: interval_update(app, text))
+
+def interval_update(app, text):
+    app.ani.event_source.stop()
+    app.run(int(text))
+
+def weight_update(app, text):
+    app.weight = float(text)
 
 if __name__ == "__main__":
-    import time
-    ge = GraphEngine()
+    client_app = ClientApp()
 
-    for _ in range(10):
-        resp = requests.get("http://127.0.0.1:8000/")
+    interval_box_ax = plt.axes([0.6, 0.30, 0.2, 0.05])
+    interval_box = widgets.TextBox(interval_box_ax, 'Interval:')
+    interval_box.on_submit(lambda text: interval_update(client_app, text))
 
-        if resp.status_code != 200:
-            raise Exception("Failed connection...")
-        for _ in range(20):
-            ge.add_data(resp.json()["data"])
+    weight_box_ax = plt.axes([0.6, 0.25, 0.2, 0.05])
+    weight_box = widgets.TextBox(weight_box_ax, "Weight: ")
+    weight_box.on_submit(lambda text: weight_update(client_app, text))
 
-            ge.degree_distribution()
-            # ge.plot_graph()
-            time.sleep(1)
-
+    client_app.run()
--- project/server.py
+++ project/server.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI
-from data import DataSource
-import pandas as pd
+from flask import Flask, jsonify
 import networkx as nx
+import random
+import time
+import pandas as pd
 
-
-
-app = FastAPI()
+app = Flask(__name__)
 
 data = pd.read_csv("../data/facebook_combined.txt", sep=" ", header=None)
 data.rename(
@@ -14,13 +13,76 @@ data.rename(
         1: "Target"},
     inplace=True)
     
-TEMP = nx.from_pandas_edgelist(data, "Source", "Target")
-handle = DataSource(TEMP, [100, 50, 30], default_sample_size=20)
+G1 = nx.from_pandas_edgelist(data, "Source", "Target")
+
+# Inicjalizacja pustego grafu G2
+G2 = nx.Graph()
+
+# Dodanie losowego podgrafu do G2
+random_nodes = random.sample(list(G1.nodes()), 10)
+G2.add_nodes_from(random_nodes)
+for node in random_nodes:
+    G2.add_node(node, weight=1)  # Waga ustawiona na 1 dla początkowych 10 węzłów
+G2.add_edges_from(G1.subgraph(random_nodes).edges())
 
 
-@app.get("/")
-def index():
-    data = handle.get_data()
+def update_external_graph():
+    # Metoda zewnętrzna - zwraca listę wierzchołków, krawędzi i wag G2
+    nodes_data = list(G2.nodes(data=True))
+    edges_data = list(G2.edges())
 
-    return {"data": data}
-#  uvicorn server:app --reload
+    # Przygotowanie słownika z danymi wierzchołków, krawędzi i wag
+    graph_data = {
+        'nodes': [{'id': node, 'weight': data.get('weight', 0)} for node, data in nodes_data],
+        'edges': edges_data
+    }
+
+    return jsonify(graph_data)
+
+
+def update_internal_graph():
+    # Metoda wewnętrzna - co 60 sekund
+    while True:
+        time.sleep(5)
+
+        # Zmiana wag przed dodaniem nowego wierzchołka
+        for node_G2 in G2.nodes:
+            G2.nodes[node_G2]['weight'] = random.uniform(max(0, G2.nodes[node_G2]['weight'] - 0.01),
+                                                         min(1, G2.nodes[node_G2]['weight'] + 0.01))
+
+        # Wybierz losowy wierzchołek z G2
+        random_node_G2 = random.choice(list(G2.nodes()))
+
+        # Wybierz losowego sąsiada tego wierzchołka z G1
+        neighbors_G1 = list(G1.neighbors(random_node_G2))
+        if neighbors_G1:
+            random_neighbor_G1 = random.choice(neighbors_G1)
+
+            # Sprawdź, czy sąsiad jest już w G2
+            if random_neighbor_G1 in G2:
+                # Jeśli tak, usuń go z G2
+                G2.remove_node(random_neighbor_G1)
+            else:
+                # Jeśli nie, dodaj go do G2 wraz z krawędziami
+                G2.add_node(random_neighbor_G1, weight=random.uniform(max(0, G2.nodes[random_node_G2]['weight'] - 0.05),
+                                                                      min(1, G2.nodes[random_node_G2]['weight'] + 0.05)))
+                G2.add_edges_from(G1.subgraph(G2.nodes()).edges())
+
+        # Usunięcie wierzchołków o wadze zerowej z G2
+        zero_weight_nodes = [node for node in G2.nodes if G2.nodes[node]['weight'] == 0]
+        G2.remove_nodes_from(zero_weight_nodes)
+
+
+# Uruchomienie wątku dla metody wewnętrznej
+import threading
+
+threading.Thread(target=update_internal_graph, daemon=True).start()
+
+
+@app.route('/get', methods=['GET'])
+def get():
+    return update_external_graph()
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
